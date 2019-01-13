b49fa8d9337607aa7d307e7ffe373c0718017e39
glittika-gerdmanee <43663001+glittika-gerdmanee@users.noreply.github.com>
2019-01-14 01:36:45 +1100

    Activate Cutscene Script added

--- Assets/scripts/activateCutscene.cs
+++ Assets/scripts/activateCutscene.cs
@@ -3,38 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
 
-public class cutToCamera : MonoBehaviour {
+public class activateCutscene : MonoBehaviour {
 
     public PlayableDirector timeline;
     public CharacterController player;
+    public new BoxCollider gameObject;
     public float countdown;
 
     // Use this for initialization
     void Start()
     {
         timeline = GetComponent<PlayableDirector>();
-
+        gameObject = GetComponent<BoxCollider>();
+        player = FindObjectOfType<CharacterController>();
     }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.tag == "Player")
-        {
-            timeline.Stop();
-        }
-    }
+  
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
             timeline.Play();
-            player.enabled = !player.enabled;
-            Destroy(gameObject, countdown);
+            Destroy(gameObject);
+            StartCoroutine(toggleMove());
         }
+    }
+
+    private IEnumerator toggleMove()
+    {
+        player.enabled = false;
+        yield return new WaitForSeconds(countdown);
+        player.enabled = true;
     }
 
     // Update is called once per frame
     void Update () {
-		
+
 	}
 }
