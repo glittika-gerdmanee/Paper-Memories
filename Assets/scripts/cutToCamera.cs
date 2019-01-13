eeee70667610d851b1583f97103ecc2eafa966a4
glittika-gerdmanee <43663001+glittika-gerdmanee@users.noreply.github.com>
2019-01-14 00:17:26 +1100

    Cut to Camera (cutscenes) script added

---
+++ Assets/scripts/cutToCamera.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class cutToCamera : MonoBehaviour {
+
+    public PlayableDirector timeline;
+    public CharacterController player;
+    public float countdown;
+
+    // Use this for initialization
+    void Start()
+    {
+        timeline = GetComponent<PlayableDirector>();
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            timeline.Stop();
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            timeline.Play();
+            player.enabled = !player.enabled;
+            Destroy(gameObject, countdown);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
