25cae7a515d036693fb4ef8f28bdaeb94e1ce370
glittika-gerdmanee <43663001+glittika-gerdmanee@users.noreply.github.com>
2019-01-15 14:19:42 +1100

    h

---
+++ Assets/scripts/loadManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadManager : MonoBehaviour {
+
+    public void loadlevel(string endScreen)
+    {
+        SceneManager.LoadScene(endScreen);
+
+    }
+}
